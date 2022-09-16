35820e9b5638d1074cd4d7f831a3f9b47cb3f08d
leojunioyuri <leojunioyuri@hotmail.com>
2022-09-16 03:08:26 -0300

    adicao arquivo lexer

---
+++ src/ExampleLexer.java
@@ -0,0 +1,27 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+import antlr.MinhaGramaticaLexer;
+
+import java.io.IOException;
+
+public class ExampleLexer {
+
+    public static void  main (String[] args){
+        String filename = "C:\\Users\\Leo\\Desktop\\projeto-pratico-compiladores\\src\\teste\\codigo.txt";
+        try{
+            CharStream input = CharStreams.fromFileName(filename);
+            MinhaGramaticaLexer lexer = new MinhaGramaticaLexer(input);
+            Token token;
+            while (!lexer._hitEOF){
+                token = lexer.nextToken();
+                System.out.println("Token: "+ token.toString());
+                System.out.println("    Lexema: "+ token.getText());
+                System.out.println("    Classe: "+lexer.getVocabulary().getDisplayName(token.getType()));
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
